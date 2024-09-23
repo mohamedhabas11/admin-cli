43945fe7a11c7c8623a7bb9c6452510c72b582e4
Mohamed habas <habas.mohamed@gmail.com>
2024-09-23 23:38:35 +0200

    add range of ports to portrecond subcommand

--- cmd/portrecon.go
+++ cmd/portrecon.go
@@ -2,23 +2,33 @@ package cmd
 
 import (
 	"admin-cli/internal"
+	"fmt"
 
 	"github.com/spf13/cobra"
 )
 
-var port int
+var portList []int
 var ip string
 
 var portReconCmd = &cobra.Command{
 	Use:   "portrecon",
-	Short: "Scan a port",
+	Short: "Scan a range of ports on a given IP address",
 	Run: func(cmd *cobra.Command, args []string) {
-		internal.ScanPort(ip, port)
+		// Scan the specified ports and get the list of open ports
+		openPorts := internal.ScanPorts(ip, portList)
+
+		// Display the results
+		if len(openPorts) > 0 {
+			fmt.Printf("Open ports on %s: %v\n", ip, openPorts)
+		} else {
+			fmt.Printf("No open ports found on %s\n", ip)
+		}
 	},
 }
 
 func init() {
-	portReconCmd.Flags().IntVarP(&port, "port", "p", 80, "Port to scan")
-	portReconCmd.Flags().StringVarP(&ip, "ip", "i", "localhost", "IP address to scan")
+	// Define the flags for ip and ports
+	portReconCmd.Flags().StringVarP(&ip, "ip", "i", "127.0.0.1", "IP address to scan")
+	portReconCmd.Flags().IntSliceVarP(&portList, "ports", "p", []int{80}, "Comma-separated list of ports to scan")
 	rootCmd.AddCommand(portReconCmd)
 }
--- internal/portscan.go
+++ internal/portscan.go
@@ -3,16 +3,44 @@ package internal
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
-// ScanPort scans a port on a given IP address
-func ScanPort(ip string, port int) {
-	target := fmt.Sprintf("%s:%d", ip, port)
-	conn, err := net.Dial("tcp", target)
-	if err != nil {
-		fmt.Println("Port", port, "is closed on", ip)
-		return
+// ScanPorts scans a range of ports on a given IP address
+// and returns a slice of open ports
+func ScanPorts(ip string, ports []int) []int {
+	var openPorts []int
+	var wg sync.WaitGroup
+	results := make(chan int)
+
+	// Function to scan a single port
+	scan := func(port int) {
+		defer wg.Done()
+		target := fmt.Sprintf("%s:%d", ip, port)
+		conn, err := net.Dial("tcp", target)
+		if err != nil {
+			return // Port is closed
+		}
+		conn.Close()
+		results <- port // Send open port to channel
 	}
-	conn.Close()
-	fmt.Println("Port", port, "is open on", ip)
+
+	// Start goroutines for each port
+	for _, port := range ports {
+		wg.Add(1)
+		go scan(port)
+	}
+
+	// Close results channel once all goroutines are done
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	// Collect results from the channel
+	for port := range results {
+		openPorts = append(openPorts, port)
+	}
+
+	return openPorts
 }
